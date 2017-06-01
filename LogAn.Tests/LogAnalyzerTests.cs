71aae35da0f7b228f632800c6b65db6b8b64d693
MonteC73 <jarekucinski@gmail.com>
2017-06-01 13:42:37 +0200

    Chapter 2 - Initial commit

---
+++ LogAn.Tests/LogAnalyzerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogAn.Tests
+{
+    [TestClass]
+    public class LogAnalyzerTests
+    {
+        [TestMethod]
+        public void IsValidFileName_BadExtension_ReturnsFalse()
+        {
+            // arrange
+            var analyzer = new LogAnalyzer();
+
+            // act
+            bool result = analyzer.IsValidLogFileName("filewithbadextension.foo");
+
+            // assert
+            Assert.IsFalse(result);
+        }
+    }
+}
