4d8907bc2a26801626a07618ccf3c1e88b180240
MonteC73 <jarekucinski@gmail.com>
2017-06-02 12:16:41 +0200

    tests for exception handling

---
+++ LogAn.Tests/Helpers/AssertException.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LogAn.Tests.Helpers
+{
+    public static class AssertException
+    {
+        public static void Throws<TException>(Action action) where TException : Exception
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(ex.GetType() == typeof (TException),
+                    "Expected exception of type " + typeof (TException) + " but type of " + ex.GetType() +
+                    " was thrown instead.");
+                return;
+            }
+            Assert.Fail("Expected exception of type " + typeof (TException) + " but no exception was thrown.");
+
+        }
+
+        public static void Throws<TException>(Action action, string expectedMessage) where TException : Exception
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(ex.GetType() == typeof(TException), 
+                    "Expected exception of type " + typeof(TException) + " but type of " + ex.GetType() + " was thrown instead.");
+                Assert.AreEqual(expectedMessage, ex.Message, 
+                    "Expected exception with a message of '" + expectedMessage + "' but exception with message of '" + ex.Message + "' was thrown instead.");
+                return;
+            }
+            Assert.Fail("Expected exception of type " + typeof(TException) + " but no exception was thrown.");
+        }
+    }
+}
--- LogAn.Tests/LogAnalyzerTests.cs
+++ LogAn.Tests/LogAnalyzerTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using LogAn.Tests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace LogAn.Tests
@@ -40,5 +41,57 @@ namespace LogAn.Tests
 
             analyzer.IsValidLogFileName(string.Empty);
         }
+
+
+        #region Microsoft recommendation for testing exception handling
+
+        // Link to article: https://msdn.microsoft.com/library/jj159340.aspx
+
+        [DataTestMethod]
+        [DataRow("")]
+        [DataRow(null)]
+        public void IsValidLogFileName_EmptyFileName_Throws(string fileName)
+        {
+            var analyzer = new LogAnalyzer();
+
+            AssertThrows<ArgumentException>( delegate 
+            {
+                analyzer.IsValidLogFileName(fileName);
+            });
+        }
+
+        internal static void AssertThrows<TException>(Action method)
+            where TException : Exception
+        {
+            try
+            {
+                method.Invoke();
+            }
+            catch (TException)
+            {
+                return;
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Wrong exception thrown: " + ex.Message);
+            }
+            Assert.Fail("No exception thrown");
+        }
+
+        #endregion
+
+        #region Helper class for testing exception and exception message
+
+        [DataTestMethod]
+        [DataRow("", "filename has to be provided")]
+        [DataRow(null, "filename has to be provided")]
+        public void IsValidLogFileName_EmptyFileName_Throws_HelperAssert(string fileName, string exMessage)
+        {
+            var analyzer = new LogAnalyzer();
+
+            AssertException.Throws<ArgumentException>(() => analyzer.IsValidLogFileName(fileName), exMessage);
+        }
+
+        #endregion
     }
 }
